9bbd4c2264fb88a0de1bffdb1bda31845f6e20d5
maztapee <91094826+maztapee@users.noreply.github.com>
2023-11-11 19:10:45 +0100

    Implemented a comparison address unwanted DOM manipulation during category edit

--- miscellaneous.js
+++ miscellaneous.js
@@ -1,30 +1,91 @@
-function clickEditEvent() {
-    const edit_click = document.querySelectorAll('.button2');
+
+if (cat_name){
+    const cat_name = document.getElementById('cat_display');
+    const comp_name = cat_name.getElementsByTagName('h2')[0].innerHTML;
+    const sub_string = comp_name.split(" ");
+    const sub_name = sub_string.slice(10).join(" ")
+
+    console.log(sub_name);
     
-    // Define a function to handle the click event
-    function handleButtonClick(event) {
-        // You can process the event here if needed
-        // ...
+    //------------------------------------------------------------------//
+    //------------------------------------------------------------------//
+    const storedEvent = null;
+    function x (){
+            const edit_button_click = document.querySelectorAll('.button2');
+            edit_button_click.forEach(element => {
+                    element.addEventListener('click', (event)=>{
+                            console.log(event);
+                            storedEvent = event;
+                            });
+                    });
 
-        // Then return the event object
-        return event;
-    }
+            };
+    x();
+    if (storedEvent){
+            console.log(storedEvent);
+            };
+    };
 
-    // Add a click event listener to each element with the class 'button2'
-    edit_click.forEach(edit_button => {
-        edit_button.addEventListener('click', function(event) {
-            const clickedEvent = handleButtonClick(event);
-            // Now, you can pass the 'clickedEvent' to another function or do something with it
-            anotherFunction(clickedEvent);
-        });
-    });
-}
+    
+    alert(`${cat_name} has been edited`);
 
-// Define another function to receive the event
-function anotherFunction(event) {
-    // Handle the event here
-    console.log('Received event:', event);
-}
 
-// Call the 'clickEditEvent' function to set up the event listeners
-clickEditEvent();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function clickEditEvent() {
+//     const edit_click = document.querySelectorAll('.button2');
+    
+//     // Define a function to handle the click event
+//     function handleButtonClick(event) {
+//         // You can process the event here if needed
+//         // ...
+
+//         // Then return the event object
+//         return event;
+//     }
+
+//     // Add a click event listener to each element with the class 'button2'
+//     edit_click.forEach(edit_button => {
+//         edit_button.addEventListener('click', function(event) {
+//             const clickedEvent = handleButtonClick(event);
+//             // Now, you can pass the 'clickedEvent' to another function or do something with it
+//             anotherFunction(clickedEvent);
+//         });
+//     });
+// }
+
+// // Define another function to receive the event
+// function anotherFunction(event) {
+//     // Handle the event here
+//     console.log('Received event:', event);
+// }
+
+// // Call the 'clickEditEvent' function to set up the event listeners
+// clickEditEvent();
--- static/todo_scripts/edit_todo.js
+++ static/todo_scripts/edit_todo.js
@@ -158,87 +158,102 @@ function editCategory(e) {
                 }
 
                 postResource(obj).then(function (editResponse) {
-
-                                if (currPage.childElementCount == 0) {
+                        if (cat_name){
+                                const catt_name = document.getElementById('cat_display');
+                                const comp_name = catt_name.getElementsByTagName('h2')[0].innerHTML;
+                                const sub_string = comp_name.split(" ");
+                                const sub_name = sub_string.slice(10).join(" ");
+                                /*
+                                sub_name is name of category todo on display todo display column
+                                cat_name is name of category retrieved through the edit button click event
+                                */
+                                if (sub_name.trim()===cat_name.trim()){
+                                //comparison to stop DOM manipulation when edited category is not on display
+                                //Investigate the unimplemented {else conditioning} 
+                                        if (currPage.childElementCount == 0) {
+                                                //if selected category to edit does not have any todo list item
+                                                delete_button.setAttribute("classname", "button4");
+                                                delete_button.dataset.removeid = editResponse['id'];
+                                                delete_button.innerHTML = "&cross;";
+                                                pTag_button.append(delete_button);
+                                                todo_list.setAttribute("id", "todo_list");
+                                                todo_span.innerHTML = new_task;
+                                                pTag_todo.append(todo_span);
+                                                input_check.dataset.id = editResponse['id'];
+                                                input_check.setAttribute("classname", "check");
+                                                input_check.setAttribute("id", "check_status");
+                                                input_check.setAttribute("type", "checkbox");
+                                                pTag_checkbox.append(input_check);
+                                                todo_description.append(pTag_button);
+                                                todo_description.append(pTag_todo);
+                                                todo_description.append(pTag_checkbox);
+                                                todo_list.appendChild(todo_description);
+                                                var list_display = document.getElementById('todo_list');
+                                                list_display.innerHTML = " ";
+                                                list_display.classList.remove("no_task");
+                                                list_display.append(todo_list);
+        
+                                        }
                                         
-                                        delete_button.setAttribute("classname", "button4");
-                                        delete_button.dataset.removeid = editResponse['id'];
-                                        delete_button.innerHTML = "&cross;";
-                                        pTag_button.append(delete_button);
-                                        todo_list.setAttribute("id", "todo_list");
-                                        todo_span.innerHTML = new_task;
-                                        pTag_todo.append(todo_span);
-                                        input_check.dataset.id = editResponse['id'];
-                                        input_check.setAttribute("classname", "check");
-                                        input_check.setAttribute("id", "check_status");
-                                        input_check.setAttribute("type", "checkbox");
-                                        pTag_checkbox.append(input_check);
-                                        todo_description.append(pTag_button);
-                                        todo_description.append(pTag_todo);
-                                        todo_description.append(pTag_checkbox);
-                                        todo_list.appendChild(todo_description);
-                                        var list_display = document.getElementById('todo_list');
-                                        list_display.innerHTML = " ";
-                                        list_display.classList.remove("no_task");
-                                        list_display.append(todo_list);
-
-                                }
-                                else {
-                                        // populating todo tasks list on the view
-                                        // ---------------------------------------------------------------------------------------------
-                                        
-                                        delete_button.setAttribute("classname", "button4");
-                                        delete_button.dataset.removeid = editResponse['todo_id'];
-                                        delete_button.innerHTML = "&cross;";
-                                        pTag_button.append(delete_button);
-                                        todo_span.innerHTML = editResponse['description'];
-                                        pTag_todo.append(todo_span);
-                                        input_check.dataset.id = editResponse['id'];
-                                        input_check.setAttribute("classname", "check");
-                                        input_check.setAttribute("id", "check_status");
-                                        input_check.setAttribute("type", "checkbox");
-                                        pTag_checkbox.append(input_check);
-                                        todo_description.append(pTag_button);
-                                        todo_description.append(pTag_todo);
-                                        todo_description.append(pTag_checkbox);
-                                        const todoList = document.getElementById('todo_list')
-                                        todoList.appendChild(todo_description);
-                                        delete_button.onclick = async function(e){
-                                                if (confirm("You are deleting a newly created task, do you want to proceed?")){
-                                                        // console.log(e);
-                                                        const todo_id = editResponse['todo_id'];                                                        
-                                                        try {
-                                                                const response = await fetch('/todos/'+ todo_id + '/delete', {
-                                                                    method: 'DELETE',
-                                                                    headers: {
-                                                                        'Content-Type': 'application/json'
+                                        else {
+                                                // adding todo items in a category on display with already existing todo list items
+                                                // ---------------------------------------------------------------------------------
+                                                
+                                                delete_button.setAttribute("classname", "button4");
+                                                delete_button.dataset.removeid = editResponse['todo_id'];
+                                                delete_button.innerHTML = "&cross;";
+                                                pTag_button.append(delete_button);
+                                                todo_span.innerHTML = editResponse['description'];
+                                                pTag_todo.append(todo_span);
+                                                input_check.dataset.id = editResponse['id'];
+                                                input_check.setAttribute("classname", "check");
+                                                input_check.setAttribute("id", "check_status");
+                                                input_check.setAttribute("type", "checkbox");
+                                                pTag_checkbox.append(input_check);
+                                                todo_description.append(pTag_button);
+                                                todo_description.append(pTag_todo);
+                                                todo_description.append(pTag_checkbox);
+                                                const todoList = document.getElementById('todo_list')
+                                                todoList.appendChild(todo_description);
+                                                delete_button.onclick = async function(e){
+                                                        if (confirm("You are deleting a newly created task, do you want to proceed?")){
+                                                                // console.log(e);
+                                                                const todo_id = editResponse['todo_id'];                                                        
+                                                                try {
+                                                                        const response = await fetch('/todos/'+ todo_id + '/delete', {
+                                                                            method: 'DELETE',
+                                                                            headers: {
+                                                                                'Content-Type': 'application/json'
+                                                                            }
+                                                                        });
+                                                                        if (response.ok) {
+                                                                                console.log("gone past delete function");
+                                                                                document.getElementById('todo_list').removeChild(todo_description);
+                                                                        }
+                                                                    } catch (error) {
+                                                                        console.error('An error occurred:', error);
                                                                     }
-                                                                });
-                                                                if (response.ok) {
-                                                                        console.log("gone past delete function");
-                                                                        document.getElementById('todo_list').removeChild(todo_description);
-                                                                }
-                                                            } catch (error) {
-                                                                console.error('An error occurred:', error);
-                                                            }
-                                                        
-                                                }
-                                                // remove by id from db
+                                                                
+                                                        }
+                                                        // remove by id from db
+                                                };
+                                                // ------------------------------------------------------------------------------------------------
+                                                // changing category name from edit response if there is any change in its initial name!
+                                                //--------------------------------------------------------------------------------------------------
+                                                const category_list = document.querySelectorAll(".list");
+                                                for (let i = 0; i < category_list.length; i++) {
+                                                        if (category_list[i].dataset.category_id == editResponse.id) { //the if condition was not met
+                                                                const atagElement = category_list[i].children[1];
+                                                                atagElement.innerHTML = editResponse.category_name;
+                                                                break;
+                                                        }
+                                                };
+        
+                                                return;
                                         };
-                                        // ------------------------------------------------------------------------------------------------
-                                        // changing category name from edit response if there is any change in its initial name!
-                                        //--------------------------------------------------------------------------------------------------
-                                        const category_list = document.querySelectorAll(".list");
-                                        for (let i = 0; i < category_list.length; i++) {
-                                                if (category_list[i].dataset.category_id == editResponse.id) { //the if condition was not met
-                                                        const atagElement = category_list[i].children[1];
-                                                        atagElement.innerHTML = editResponse.category_name;
-                                                        break;
-                                                }
-                                        };
+                                }
 
-                                        return;
-                                };
+                        };
                         });
 
 
